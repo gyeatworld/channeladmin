c951e4007ccb2a921a783061e3cd178a445c2ec0
vem <gyeatworld@163.com>
2017-08-15 16:16:03 +0800

    Add sencha extjs6.5.1 to project
    Add sencha extjs6.5.1 to project

---
+++ src/main/webapp/app.js
@@ -0,0 +1,18 @@
+/*
+ * This file launches the application by asking Ext JS to create
+ * and launch() the Application class.
+ */
+Ext.application({
+    extend: 'ChannelAdmin.Application',
+
+    name: 'ChannelAdmin',
+
+    requires: [
+        // This will automatically load all classes in the ChannelAdmin namespace
+        // so that application classes do not need to require each other.
+        'ChannelAdmin.*'
+    ],
+
+    // The name of the initial view to create.
+    mainView: 'ChannelAdmin.view.main.Main'
+});
---
+++ src/main/webapp/app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('ChannelAdmin.model.Personnel', {
+    extend: 'ChannelAdmin.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
